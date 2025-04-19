484d9b932034882bb183fb410124ed0d19491f3d
Ian Skelskey <ianskelskey@gmail.com>
2025-04-19 00:01:32 -0400

    Extend Patron Bucket Module with additional components
    - Added new components to the PatronBucketModule:
      - PatronBucketItemComponent
      - PatronBucketPendingComponent
      - PatronBucketContentComponent
      - PatronBucketAddDialogComponent
    - Implemented new routes with a dedicated PatronBucketRoutingModule for better modularity.
    
    Release-Note: Enhance Patron Bucket Module with additional components and routing.
    
    Signed-off-by: Ian Skelskey <ianskelskey@gmail.com>

--- Open-ILS/src/eg2/src/app/staff/circ/patron/bucket/patron-bucket.module.ts
+++ Open-ILS/src/eg2/src/app/staff/circ/patron/bucket/patron-bucket.module.ts
@@ -1,10 +1,14 @@
 import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {PatronBucketComponent} from './patron-bucket.component';
+import {PatronBucketItemComponent} from './patron-bucket-item.component';
+import {PatronBucketPendingComponent} from './patron-bucket-pending.component';
+import {PatronBucketContentComponent} from './patron-bucket-content.component';
+import {PatronBucketAddDialogComponent} from './patron-bucket-add-dialog.component';
 import {PatronBucketService} from './patron-bucket.service';
+import {PatronBucketRoutingModule} from './patron-bucket-routing.module';
 
 import {EgCommonModule} from '@eg/common.module';
 import {StaffCommonModule} from '@eg/staff/common.module';
@@ -12,19 +16,16 @@ import {GridModule} from '@eg/share/grid/grid.module';
 import {FmRecordEditorModule} from '@eg/share/fm-editor/fm-editor.module';
 import {CommonWidgetsModule} from '@eg/share/common-widgets.module';
 
-const routes: Routes = [
-    {
-        path: '',
-        component: PatronBucketComponent
-    }
-];
-
 @NgModule({
     declarations: [
-        PatronBucketComponent
+        PatronBucketComponent,
+        PatronBucketItemComponent,
+        PatronBucketPendingComponent,
+        PatronBucketContentComponent,
+        PatronBucketAddDialogComponent
     ],
     imports: [
-        RouterModule.forChild(routes),
+        PatronBucketRoutingModule,
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
@@ -32,11 +33,11 @@ const routes: Routes = [
         StaffCommonModule,
         GridModule,
         FmRecordEditorModule,
-        CommonWidgetsModule,  // This provides common dialog components
+        CommonWidgetsModule
     ],
     providers: [
         PatronBucketService
     ],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]  // This will allow custom elements like eg-bucket-dialog
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class PatronBucketModule {}
