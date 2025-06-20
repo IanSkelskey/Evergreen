d3d3a19189cdb34ecf41771ed6ba714c9f03ecb7
Ian Skelskey <ianskelskey@gmail.com>
2025-06-20 17:12:04 -0400

    Add OPAC footer component with styles and update module declarations

---
+++ Open-ILS/src/eg2/src/app/opac/opac-footer.component.ts
@@ -0,0 +1,9 @@
+// New footer component for OPAC
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eg-opac-footer',
+  templateUrl: './opac-footer.component.html',
+  styleUrls: ['./opac-footer.component.css']
+})
+export class OpacFooterComponent {}
--- Open-ILS/src/eg2/src/app/opac/opac.module.ts
+++ Open-ILS/src/eg2/src/app/opac/opac.module.ts
@@ -6,9 +6,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { OpacRoutingModule } from './routing.module';
 import { OpacComponent } from './opac.component';
 import { OpacNavComponent } from './opac-nav.component';
+import { OpacFooterComponent } from './opac-footer.component';
 
 @NgModule({
-  declarations: [OpacComponent, OpacNavComponent],
+  declarations: [OpacComponent, OpacNavComponent, OpacFooterComponent],
   imports: [CommonModule, OpacRoutingModule, RouterModule, NgbModule]
 })
 export class OpacModule {}
